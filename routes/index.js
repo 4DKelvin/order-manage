e23d0802a9e3eef4e731b31b2cdba12240135c11
Kelvin <yifeng.liang@accenture.com>
2018-03-28 12:39:25 +0800

    fast commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '订单管理' });
 });
 
 module.exports = router;
