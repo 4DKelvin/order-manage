1880ea08be09bfae361b98dfe56baac9e3b51d0e
Kelvin <yifeng.liang@accenture.com>
2018-04-17 17:23:19 +0800

    fast commit

--- routes/index.js
+++ routes/index.js
@@ -69,23 +69,8 @@ router.get('/order_by_oid', function (req, res) {
 });
 
 router.get('/order', function (req, res, next) {
-    console.log('http://fuwu.jiulvxing.com/autoOta/orderExport?' + querystring.stringify({
-            domain: 'yfh',
-            user: '17373761393',
-            pass: '741852',
-            type: 'incr',
-            start: req.query.start,
-            end: req.query.end
-        }));
     request.post({
-        url: 'http://fuwu.jiulvxing.com/autoOta/orderExport?' + querystring.stringify({
-            domain: 'yfh',
-            user: '17373761393',
-            pass: '741852',
-            type: 'incr',
-            start: req.query.start,
-            end: req.query.end
-        })
+        url: 'http://fuwu.jiulvxing.com/autoOta/orderExport?domain=yfh&user=17373761393&pass=741852&type=incr&start=' + req.query.start + '&end=' + req.query.end
     }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.json({res: body});
