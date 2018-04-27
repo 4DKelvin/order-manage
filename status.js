945d1c519aad98727666b94e655260a3b3874dbc
Kelvin <yifeng.liang@accenture.com>
2018-04-27 19:31:24 +0800

    fast commit

---
+++ status.js
@@ -0,0 +1,9 @@
+const request = require('request');
+const utils = request('./utils');
+setInterval(function () {
+    utils.refreshAll().then(function (r) {
+        console.log(r);
+    }, function (e) {
+        console.log(e);
+    })
+}, 1000);
