a2da5256d9856c83ba43b6b406c2e0b9f4ebc7ff
Kelvin <yifeng.liang@accenture.com>
2018-04-27 22:00:27 +0800

    fast commit

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const utils = require('../utils');
-
-
+const n = function (num) {
+    if (Number(num) < 10) {
+        return '0' + num;
+    }
+    return num;
+};
+const format = function (date) {
+    var d = new Date(Date.parse(decodeURIComponent(date)));
+    return [d.getFullYear() + n(d.getMonth() + 1), n(d.getDate())].join('-') + ' ' +
+        [n(d.getHours()), n(d.getMinutes()), n(d.getSeconds())].join(':');
+};
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    utils.orders(decodeURIComponent(req.query.start), decodeURIComponent(req.query.end), req.query.page || 0).then(function (r) {
+    var d = new Date(),
+        start = [d.getFullYear() + n(d.getMonth() + 1), n(d.getDate())].join('-') + ' 00:00:00',
+        end = [d.getFullYear() + n(d.getMonth() + 1), n(d.getDate())].join('-') + ' 23:59:59';
+    utils.orders(format(req.query.start), format(req.query.end), req.query.page || 0).then(function (r) {
         res.render('index', {
             title: '订单管理',
             data: JSON.parse(JSON.stringify(r)),
             params: {
-                start: decodeURIComponent(req.query.start),
-                end: decodeURIComponent(req.query.end),
+                start: format(req.query.start) || start,
+                end: format(req.query.end) || end,
                 page: req.query.page
             }
         });
@@ -20,12 +32,28 @@ router.get('/', function (req, res, next) {
             title: '订单管理',
             data: [],
             params: {
-                start: decodeURIComponent(req.query.start),
-                end: decodeURIComponent(req.query.start),
+                start: format(req.query.start) || start,
+                end: format(req.query.end) || end,
                 page: req.query.page
             }
         });
     });
+});
+
+router.get('/bind', function (req, res, next) {
+    utils.bindUser(req.query.order_id).then(function (r) {
+        res.json(r);
+    }, function (e) {
+        res.json(e);
+    })
+});
+
+router.get('/order', function (req, res, next) {
+    utils.placeOrder(req.query.order_id, req.query.price).then(function (r) {
+        res.json(r);
+    }, function (e) {
+        res.json(e);
+    })
 });
 
 router.get('/refresh', function (req, res, next) {
