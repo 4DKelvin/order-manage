3dfcbcd99ec07cd3140b0e0c683fee9d99b0a7c7
Kelvin <yifeng.liang@accenture.com>
2018-04-27 19:09:22 +0800

    fast commit

--- db.js
+++ db.js
@@ -1,31 +1,8 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({});
-mongoose.connect('mongodb://localhost/orders')
-const OrderModel = mongoose.model('Order', schema);
-const ConfigModel = mongoose.model('Config', schema);
+mongoose.connect('mongodb://localhost/orders');
 
 module.exports = {
-    Order: OrderModel,
-    Config: ConfigModel
+    Order: mongoose.model('Order', schema),
+    User: mongoose.model('User', schema)
 };
-
-//setInterval(function () {
-//    var date = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-'),
-//        start_time = date + [new Date().getHours(), new Date().getMinutes(), '00'].join(':'),
-//        end_time = date + [new Date().getHours(), new Date().getMinutes(), '00'].join(':');
-//    request.post({
-//        url: 'http://fuwu.jiulvxing.com/autoOta/orderExport?domain=yfh&user=17373761393&pass=741852&type=incr&start=' +
-//        start_time + '&end=' + end_time
-//    }, function (error, response, body) {
-//        if (!error && response.statusCode == 200) {
-//            var res = xml2json.parser(body);
-//            if (!res.result.status == 'error') {
-//                res.result.order.forEach(function (e) {
-//                    for (var key in e) {
-//                        e[key]
-//                    }
-//                })
-//            }
-//        }
-//    });
-//}, 1000);
--- order.js
+++ order.js
@@ -1,10 +1,12 @@
 const request = require('request');
 const Order = require('./db').Order;
 const xml2json = require('node-xml2json');
-setInterval(function () {
+//setInterval(function () {
     var date = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('-'),
         start_time = date + [new Date().getHours(), new Date().getMinutes(), '00'].join(':'),
         end_time = date + [new Date().getHours(), new Date().getMinutes(), '00'].join(':');
+    start_time = '2018-4-1 00:00:00';
+    end_time = '2018-4-1 23:59:59';
     request.post({
         url: 'http://fuwu.jiulvxing.com/autoOta/orderExport?domain=yfh&user=17373761393&pass=741852&type=incr&start=' +
         start_time + '&end=' + end_time
@@ -17,9 +19,11 @@ setInterval(function () {
                     for (var key in e) {
                         order[key] = e[key];
                     }
-                    new Order(order).save();
+                    Order.update({id: order.id}, order, {upsert: true}, function () {
+
+                    });
                 })
             }
         }
     });
-}, 30000);
+//}, 30000);
