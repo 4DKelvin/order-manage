37f6a027abf6dc3928ab092268847b5deb665e1b
Kelvin <yifeng.liang@accenture.com>
2018-05-20 22:06:20 +0800

    fast commit

--- state.js
+++ state.js
@@ -1,7 +1,7 @@
 const api = require('./utils');
 const xml2json = require('node-xml2json');
 // setInterval(function() {
-api._state('TAO', 'XIY', '2018/05/15').then(function(r) {
+api._state('CTU', 'SHA', '2018-05-28').then(function(r) {
         var res = xml2json.parser(r.data);
         console.log(res);
     }, function(e) {
