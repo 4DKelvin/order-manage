ae85136d65567acc3ffefe52956b8f0eb964777f
Kelvin <yifeng.liang@accenture.com>
2018-05-20 23:28:33 +0800

    fast commit

--- state.js
+++ state.js
@@ -12,9 +12,7 @@ api._state('CTU', 'SZX', '2018-05-28').then(function(r) {
                         (values[7] || '').replace(/(\S\S)/g, function(e) {
                             var val = e.replace(/([a-zA-Z])([0-9])/g, '$1 $2');
                             if (val.indexOf(' ') >= 0) {
-                                var spaces = {},
-                                    vals = val.split(' ');
-                                spaces[vals[0]] = vals[1];
+                                var vals = val.split(' ');
                                 res_data.push({
                                     date: values[0],
                                     dep_time: values[1],
@@ -22,12 +20,17 @@ api._state('CTU', 'SZX', '2018-05-28').then(function(r) {
                                     flight_no: values[3] + values[4],
                                     dep_city: values[5],
                                     arr_city: values[6],
-                                    spaces: spaces
+                                    spaces: {
+                                        num: vals[0],
+                                        count: vals[1]
+                                    }
                                 });
                             }
                         });
                     }
-                    console.dir(res_data);
+                    console.dir(res_data.sort(function(a, b) {
+                        return a.spaces.count - b.spaces.count;
+                    }));
                 });
             });
         },
