20133f6e49baf5e4eaa25871bb72e7ce5dc0ebfe
Kelvin <yifeng.liang@accenture.com>
2018-05-23 07:03:07 +0800

    fast commit

--- timesupdate.js
+++ timesupdate.js
@@ -6,4 +6,4 @@ setInterval(function() {
         }).join('+'));
         console.log('[' + new Date().toDateString() + '] Sync finish, space database has been ' + nModified + ' rows updated.');
     })
-}, 15000);
+}, 5000);
