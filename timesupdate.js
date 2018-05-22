e574410176c829388ea5838fcb8efb15c2d0eafe
Kelvin <yifeng.liang@accenture.com>
2018-05-23 05:42:50 +0800

    fast commit

--- timesupdate.js
+++ timesupdate.js
@@ -1,9 +1,9 @@
 const api = require('./utils');
-api.get_sync_space().then(function(r) {
-    var nModified = eval(r.map(function(e) {
-        return e.nModified;
-    }).join('+'));
-    console.log(nModified + ' rows updated.');
-}, function(e) {
-    console.error(e);
-})
+setInterval(function() {
+    api.get_sync_space().then(function(r) {
+        var nModified = eval(r.map(function(e) {
+            return e.nModified;
+        }).join('+'));
+        console.log('[' + new Date().toDateString() + ']' + nModified + 'rows updated.');
+    })
+}, 1000);
