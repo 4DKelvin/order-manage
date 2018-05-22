0c616fdfd76f7502737153b1f0f168a5fd12864c
Kelvin <yifeng.liang@accenture.com>
2018-05-23 05:28:17 +0800

    fast commit

--- state.js
+++
@@ -1,9 +0,0 @@
-const api = require('./utils');
-api.get_sync_space().then(function(r) {
-    var nModified = eval(r.map(function(e) {
-        return e.nModified;
-    }).join('+'));
-    console.log(r, nModified);
-}, function(e) {
-    console.error(e);
-})
