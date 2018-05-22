3c2c1a56d44c33fec1f24c5fb8606ef7349c0b41
Kelvin <yifeng.liang@accenture.com>
2018-05-23 03:29:31 +0800

    fast commit

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ const format = function(date) {
 };
 const formatTime = function(date) {
     var d = date ? new Date(date) : new Date();
-    return [n(d.getHours()), n(d.getMinutes()), n(d.getSeconds())].join(':')
+    return [n(d.getUTCHours()), n(d.getUTCMinutes()), n(d.getUTCSeconds())].join(':')
 };
 const formatDate = function(date) {
     var d = date ? new Date(Date.parse(decodeURIComponent(date))) : new Date();
