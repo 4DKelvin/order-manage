be8210b59585791c894483ab089f67e92657c697
Kelvin <yifeng.liang@accenture.com>
2018-05-23 18:26:23 +0800

    fast commit

--- utils.js
+++ utils.js
@@ -466,7 +466,6 @@ var api = {
                     else {
                         api.spaces_remote(space.dep_city, space.arr_city, space.flight_date)
                             .then(function(data) {
-                                console.log(data);
                                 Promise.all(data.map(function(item) {
                                     return Space.update({
                                         flight_no: item.flight_no.toUpperCase(),
@@ -504,7 +503,6 @@ var api = {
         return new Promise(function(resolve, reject) {
             api._state(dep, arr, date).then(function(r) {
                     parseString(r, function(err, res) {
-                        console.log(res);
                         var data = res.string._,
                             items = data.split(',E#'),
                             res_data = [];
@@ -533,7 +531,6 @@ var api = {
                     });
                 },
                 function(e) {
-                    console.log(e);
                     resolve([]);
                 });
         });
