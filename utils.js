35ce1ab149b8a859758824927d12ce567be7ecc2
Kelvin <yifeng.liang@accenture.com>
2018-05-24 19:29:22 +0800

    fast commit

--- utils.js
+++ utils.js
@@ -408,6 +408,7 @@ var api = {
                 dep_time: space.dep_time,
                 space_name: space.space_name
             }, function(err, item) {
+                console.log(err, item);
                 if (err) reject(err);
                 else if (item) reject('订单已存在');
                 else {
